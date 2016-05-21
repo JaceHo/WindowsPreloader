3540f8d9cbe68a614f4982bd18d2030cccd032ab
hippo <895196292@qq.com>
2016-05-21 17:52:47 +0800

    using keyframe to animation windowspreloader

--- library/src/main/java/info/futureme/preloader/CircleView.java
+++ library/src/main/java/info/futureme/preloader/CircleView.java
@@ -22,7 +22,6 @@ public class CircleView extends View{
         paint.setAntiAlias(true);
         paint.setColor(color);
         paint.setStyle(Paint.Style.FILL_AND_STROKE);
-        paint.setStrokeWidth(radis / 8);
     }
 
     @Override
@@ -34,10 +33,10 @@ public class CircleView extends View{
     public void layout(int l, int t, int r, int b){
         l += paddingLeft;
         t += paddingTop;
-        super.layout(l , t, l + 2 * radis, t + radis * 2);
+        super.layout(l , t + radis, l + 2 * radis, t + radis * 3);
     }
 
     public void onDraw(Canvas canvas){
-        canvas.drawCircle((float)(getLeft() - paddingLeft + radis), (float)(getTop() - paddingTop + radis), radis, paint);
+        canvas.drawCircle((float)(getLeft() - paddingLeft + radis), (float)(getTop() - paddingTop ), radis, paint);
     }
 }
--- library/src/main/java/info/futureme/preloader/WindowsPreloaderView.java
+++ library/src/main/java/info/futureme/preloader/WindowsPreloaderView.java
@@ -11,22 +11,23 @@ import android.animation.PropertyValuesHolder;
 import android.animation.TimeInterpolator;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
 import android.graphics.Color;
-import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
-import android.view.ViewTreeObserver;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import info.futureme.preloader.util.EasingUtil;
+
 
 /**
- * Created by daimajia on 14-4-30.
+ * Created by jeffrey on 14-4-30.
  */
-public class WindowsPreloaderView extends RelativeLayout implements ViewTreeObserver.OnGlobalLayoutListener {
+public class WindowsPreloaderView extends RelativeLayout {
 
 
     /**
@@ -52,16 +53,36 @@ public class WindowsPreloaderView extends RelativeLayout implements ViewTreeObse
 
     private WindowsPreloaderTimeInterpolator windowsPreloaderTimeInterpolator;
 
+
+    /**
+     * circleview dots to be animated
+     */
     private List<CircleView> dots;
 
-    PropertyValuesHolder alphaHolder = PropertyValuesHolder.ofFloat(View.ALPHA, 1, 1, 1, 1, 1, 1, 0, 0);
-    PropertyValuesHolder rotateHolder = PropertyValuesHolder.ofFloat(View.ROTATION, 225, 345, 455, 690, 815, 945, 945, 945);
-    Keyframe[] keyframes = new Keyframe[]{
-            Keyframe.ofFloat(0.0f), Keyframe.ofFloat(0.07f),
-            Keyframe.ofFloat(0.3f), Keyframe.ofFloat(0.39f),
-            Keyframe.ofFloat(0.7f), Keyframe.ofFloat(0.75f),
-            Keyframe.ofFloat(0.76f), Keyframe.ofFloat(1.0f)
-    };
+    /**
+     * animation set
+     */
+    AnimatorSet set = new AnimatorSet();
+
+    /**
+     * keyframe alpha holder
+     */
+    PropertyValuesHolder alphaHolder = PropertyValuesHolder.ofKeyframe(View.ALPHA,
+            Keyframe.ofFloat(0.0f, 1), Keyframe.ofFloat(0.07f, 1),
+            Keyframe.ofFloat(0.3f, 1), Keyframe.ofFloat(0.39f, 1),
+            Keyframe.ofFloat(0.7f, 1), Keyframe.ofFloat(0.75f, 1),
+            Keyframe.ofFloat(0.76f, 0), Keyframe.ofFloat(1.0f, 0)
+    );
+
+    /**
+     * keyframe rotateholder
+     */
+    PropertyValuesHolder rotateHolder = PropertyValuesHolder.ofKeyframe(View.ROTATION,
+            Keyframe.ofFloat(0.0f, 180), Keyframe.ofFloat(0.07f, 300),
+            Keyframe.ofFloat(0.3f, 410), Keyframe.ofFloat(0.39f, 645),
+            Keyframe.ofFloat(0.7f, 770), Keyframe.ofFloat(0.75f, 900),
+            Keyframe.ofFloat(0.76f, 900), Keyframe.ofFloat(1.0f, 900)
+    );
 
     public WindowsPreloaderView(Context context) {
         this(context, null);
@@ -83,8 +104,6 @@ public class WindowsPreloaderView extends RelativeLayout implements ViewTreeObse
         mDotColor = attributes.getColor(R.styleable.WindowsPreloader_dot_color, default_dot_color);
         mDotHeight = attributes.getDimension(R.styleable.WindowsPreloader_dot_height, default_dot_height);
 
-
-        getViewTreeObserver().addOnGlobalLayoutListener(this);
         attributes.recycle();
     }
 
@@ -101,12 +120,41 @@ public class WindowsPreloaderView extends RelativeLayout implements ViewTreeObse
     @Override
     public void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
+        if (dots == null) {
+            dots = new ArrayList<>();
+            for (int i = 0; i < 5; i++) {
+                final CircleView circleView = new CircleView(getContext(), mDotColor, (int) (mDotHeight / 2), getMeasuredWidth() / 2 - (int) (mDotHeight / 2), getPaddingTop());
+                circleView.setAlpha(0);// initial it is transparent
+                dots.add(circleView);
+                circleView.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+                circleView.setPivotY(getMeasuredHeight() / 2 - getPaddingTop() - mDotHeight / 2);
+                circleView.setPivotX(mDotHeight / 2);
+                addView(circleView);
+
+                final ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(circleView, rotateHolder, alphaHolder);
+                animator.setInterpolator(windowsPreloaderTimeInterpolator);
+                animator.setStartDelay(240 * i);
+                animator.setDuration(ANIMATION_DURATION);
+                animator.setRepeatCount(-1);
+
+                set.playTogether(animator);
+            }
+        }
+
+    }
+
+    @Override
+    public void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        //start animation
+        set.start();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         setMeasuredDimension(measure(widthMeasureSpec, true), measure(heightMeasureSpec, false));
     }
+
 
     private int measure(int measureSpec, boolean isWidth) {
         int result;
@@ -129,38 +177,6 @@ public class WindowsPreloaderView extends RelativeLayout implements ViewTreeObse
         return result;
     }
 
-    @Override
-    public void onGlobalLayout() {
-        if (dots == null) {
-            AnimatorSet set = new AnimatorSet();
-            dots = new ArrayList<>();
-            for (int i = 0; i < 5; i++) {
-                CircleView circleView = new CircleView(getContext(), mDotColor, (int) (mDotHeight / 2), getMeasuredWidth() / 2 - (int) (mDotHeight / 2), getPaddingTop());
-                circleView.setAlpha(1);// initial it is transparent
-                dots.add(circleView);
-                circleView.setLayoutParams(new LinearLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
-                circleView.setPivotY(getMeasuredHeight() / 2 - getPaddingTop() - mDotHeight / 2);
-                circleView.setPivotX(mDotHeight / 2);
-                addView(circleView);
-
-                rotateHolder.setKeyframes(keyframes);
-                alphaHolder.setKeyframes(keyframes);
-                final ObjectAnimator animator = ObjectAnimator.ofPropertyValuesHolder(circleView, rotateHolder, alphaHolder);
-                animator.setInterpolator(windowsPreloaderTimeInterpolator);
-                animator.setStartDelay(240 * i);
-                animator.setDuration(ANIMATION_DURATION);
-                animator.setRepeatCount(-1);
-                set.playTogether(animator);
-            }
-            set.start();
-        }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-            getViewTreeObserver().removeOnGlobalLayoutListener(this);
-        } else {
-            getViewTreeObserver().removeGlobalOnLayoutListener(this);
-        }
-    }
 
     /**
      * Created by hippo on 5/15/16.
@@ -170,25 +186,33 @@ public class WindowsPreloaderView extends RelativeLayout implements ViewTreeObse
         // time -> animation progress
         @Override
         public float getInterpolation(float input) {
-            float fraction = 0;
-            if (input < 0.07f) {
-                float domain = 0.07f;
-                float duration = input * domain;
-                fraction = domain * ((input = input / duration - 1) * input * input + 1) + 0.07f;
-            } else if (input < 0.3f) {
-                fraction = input;
-            } else if (input < 0.39f) {
-                float domain = 0.7f - 0.39f;
-                float duration = input * domain;
-                fraction = ((input /= duration / 2) < 1.0f) ?
-                        (domain / 2 * input * input * input + 0.39f)
-                        : (domain / 2 * ((input -= 2) * input * input + 2) + 0.39f);
-            } else if (input < 0.7f) {
-                fraction = input;
+            //DOMAIN*f((x-pre)/DURATION)+START
+            float fraction;
+            if (input <= 0.07f) {
+                EasingUtil.START = 0.0f;
+                EasingUtil.DURATION = 0.07f;
+                EasingUtil.DOMAIN = EasingUtil.DURATION;
+                fraction = EasingUtil.Cubic.easeOut.getInterpolation(input);
+            } else if (input <= 0.3f) {
+                EasingUtil.START = getInterpolation(0.07f);
+                EasingUtil.DURATION = 0.3f - 0.07f;
+                EasingUtil.DOMAIN = EasingUtil.DURATION;
+                fraction = EasingUtil.Linear.easeNone.getInterpolation(input - 0.07f);
+            } else if (input <= 0.39f) {
+                EasingUtil.START = getInterpolation(0.3f);
+                EasingUtil.DURATION = 0.39f - 0.3f;
+                EasingUtil.DOMAIN = EasingUtil.DURATION;
+                fraction = EasingUtil.Cubic.easeInOut.getInterpolation(input -0.3f);
+            } else if (input <= 0.7f) {
+                EasingUtil.START = getInterpolation(0.39f);
+                EasingUtil.DURATION = 0.7f - 0.39f;
+                EasingUtil.DOMAIN = EasingUtil.DURATION;
+                fraction = EasingUtil.Linear.easeNone.getInterpolation(input - 0.39f);
             } else {
-                float domain = 1.0f - 0.7f;
-                float duration = ANIMATION_DURATION * domain;
-                fraction = domain * ((input = input / duration - 1) * input * input + 1) + 0.7f;
+                EasingUtil.START = getInterpolation(0.7f);
+                EasingUtil.DURATION = 1.0f - 0.7f;
+                EasingUtil.DOMAIN = EasingUtil.DURATION;
+                fraction = EasingUtil.Cubic.easeOut.getInterpolation(input - 0.7f);
             }
             return fraction;
         }
